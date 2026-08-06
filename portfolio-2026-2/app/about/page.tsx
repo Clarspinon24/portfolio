76e591d431a18092025ad8c8385a0c2207a7731d
Clara Marchal <marchal.clara95@gmail.com>
2026-08-06 17:02:21 +0200

    fix: about page

--- portfolio-2026-2/app/about/page.tsx
+++ portfolio-2026-2/app/about/page.tsx
@@ -8,16 +8,11 @@ export default function Parcours() {
 
   <h1>About me</h1>
   
+  <img className={styles.image} src="asset/music.png" alt="" />
 
-
-
-
+  <p className={styles.text}>Hello, my name is Clara</p>
     
 
     </> // Fin du fragment obligatoire
   );
 }
-// /* 
-//   Parcours professionnel -> 
-//   Compétences détaillées ->
-// */
